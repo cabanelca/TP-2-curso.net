af87412291a5476b9221aecf661cb40fa7685447
Ruben O Cabanellas <rucaba14@gmail.com>
2022-03-26 17:14:29 -0300

    ejercicio 1y 2 del trabajo

---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcularIva
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         
+                double producto;
+                double resultado;
+
+                Console.WriteLine("Igrese un monto de un producto");
+                producto = int.Parse(Console.ReadLine());
+
+                resultado = producto * 0.21;
+                Console.WriteLine("El IVA es " + resultado);
+                Console.WriteLine("El valor final es " + (resultado + producto));
+                Console.ReadLine();
+            
+        
+
+        }
+    }
+}
---
+++ Ejercicio2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nro= 1;
+            while (nro <= 100)
+            {
+                Console.WriteLine(nro);
+                nro+= 2;
+            }
+                 Console.ReadKey();
+            }
+    }
+}
