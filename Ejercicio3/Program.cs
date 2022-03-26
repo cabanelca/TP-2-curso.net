46b4c0280533ac2d40c4d828bfb7f19d28f93992
Ruben O Cabanellas <rucaba14@gmail.com>
2022-03-26 17:42:31 -0300

    se agrega ejercicio 3

---
+++ Ejercicio3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio3
+{
+    internal class Program
+    {
+        static void CalcularIva(int numero)
+        {
+            
+            double resultado;
+
+            
+            resultado = numero * 1.21;
+            Console.WriteLine("El valor final es " + (resultado));
+            Console.ReadLine();
+        }
+       static void Main (string[] args)
+        {
+            Console.WriteLine("Igrese un monto de un producto");
+            CalcularIva(int.Parse(Console.ReadLine()));
+           
+        }
+
+
+
+    }
+}
